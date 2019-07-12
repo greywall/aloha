f1b2f6955379827d0d8cf4f071e8474f9adc68eb
Tal Grewal <talwindergrewal01@gmail.com>
2019-07-11 22:07:57 -0700

    2019-07-11 Carousel Added and Working

---
+++ flickity.js
@@ -0,0 +1,7 @@
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true,
+});
---
+++ javascript.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+
+const button = document.getElementById("email-form");
+button.addEventListener("submit", function() {
+  console.log("You clicked a button!"); prompt("Thanks for providing us your email address.")
+});
+
+
+// Flicity Carosal
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true
+});
+
+
+
+// Scroll  
+
+
+
+
+
+});
